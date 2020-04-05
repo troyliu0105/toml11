70d090346ad0cc892cda3f4f462c29afc94eb034
ToruNiina <niina.toru.68u@gmail.com>
2020-04-05 16:46:13 +0900

    test: add test of string's member methods

--- tests/test_string.cpp
+++ tests/test_string.cpp
@@ -148,7 +148,100 @@ BOOST_AUTO_TEST_CASE(test_string_add_assign)
         str += str2;
         BOOST_TEST(str == "foobar");
     }
+}
 
+// check commonly-used member methods
 
+BOOST_AUTO_TEST_CASE(test_string_size_related_methods)
+{
+    {
+        toml::string str(std::string("foobar"));
+
+        BOOST_TEST(str.size()   == 6u);
+        BOOST_TEST(str.length() == 6u);
+        BOOST_TEST(str.empty()  == false);
+
+        BOOST_TEST(str.max_size() == str.str.max_size());
+        BOOST_TEST(str.capacity() == str.str.capacity());
+
+        str.clear();
+
+        BOOST_TEST(str.size()   == 0u);
+        BOOST_TEST(str.length() == 0u);
+        BOOST_TEST(str.empty()  == true);
+
+        str.shrink_to_fit(); // check it compiles
+
+        // after shrnik_to_fit, still capacity is not zero. It is because of
+        // short string optimization (SSO). Short string can be statically
+        // stored in the std::string class without any dynamic allocation.
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_string_iterator_related_methods)
+{
+    {
+        toml::string str(std::string("foobar"));
+
+        std::string copied(str.size(), ' ');
+        std::copy(str.begin(), str.end(), copied.begin());
+        BOOST_TEST(copied == str.str);
+    }
+    {
+        const toml::string str(std::string("foobar"));
+
+        std::string copied(str.size(), ' ');
+        std::copy(str.begin(), str.end(), copied.begin());
+        BOOST_TEST(copied == str.str);
+    }
+
+    {
+        toml::string str(std::string("foobar"));
+
+        std::string copied(str.size(), ' ');
+        std::copy(str.rbegin(), str.rend(), copied.rbegin());
+        BOOST_TEST(copied == str.str);
+    }
+    {
+        const toml::string str(std::string("foobar"));
+
+        std::string copied(str.size(), ' ');
+        std::copy(str.rbegin(), str.rend(), copied.rbegin());
+        BOOST_TEST(copied == str.str);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_string_element_access)
+{
+    {
+        toml::string str(std::string("foobar"));
+
+        BOOST_TEST(str.front() == 'f');
+        BOOST_TEST(str.at(1)   == 'o');
+        BOOST_TEST(str[2]      == 'o');
+        BOOST_TEST(str.at(3)   == 'b');
+        BOOST_TEST(str[4]      == 'a');
+        BOOST_TEST(str.back()  == 'r');
+    }
+    {
+        const toml::string str(std::string("foobar"));
+
+        BOOST_TEST(str.front() == 'f');
+        BOOST_TEST(str.at(1)   == 'o');
+        BOOST_TEST(str[2]      == 'o');
+        BOOST_TEST(str.at(3)   == 'b');
+        BOOST_TEST(str[4]      == 'a');
+        BOOST_TEST(str.back()  == 'r');
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_string_substr)
+{
+    {
+        const toml::string str(std::string("foobar"));
+
+        BOOST_TEST(str.substr(0, 3) == "foo");
+        BOOST_TEST(str.substr(3, 3) == "bar");
+    }
 }
 
