31c83c1e069e739eeafc8721d1d91a460dfa5e45
Troy Liu <troyliu0105@outlook.com>
2023-04-26 11:09:54 +0800

    F: fix rk3588 compilation under 4.9 gnustl

--- toml/region.hpp
+++ toml/region.hpp
@@ -11,6 +11,101 @@
 #include <cassert>
 #include "color.hpp"
 
+// fix rk3588 4.9 gnustl_static
+#if (__GNUC__==4 && __GNUC_MINOR__==9) && defined(RM_ANDROID)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ext/string_conversions.h>
+namespace std _GLIBCXX_VISIBILITY(default) {
+_GLIBCXX_BEGIN_NAMESPACE_VERSION
+
+using ::strtol;
+using ::strtoll;
+using ::strtoul;
+using ::strtoull;
+using ::strtof;
+using ::strtold;
+using ::vsnprintf;
+using ::snprintf;
+
+// 21.4 Numeric Conversions [string.conversions].
+inline int stoi(const string &__str, size_t *__idx = 0, int __base = 10) {
+  return __gnu_cxx::__stoa<long, int>(&std::strtol, "stoi", __str.c_str(), __idx, __base);
+}
+
+inline long stol(const string &__str, size_t *__idx = 0, int __base = 10) {
+  return __gnu_cxx::__stoa(&std::strtol, "stol", __str.c_str(), __idx, __base);
+}
+
+inline unsigned long stoul(const string &__str, size_t *__idx = 0, int __base = 10) {
+  return __gnu_cxx::__stoa(&std::strtoul, "stoul", __str.c_str(), __idx, __base);
+}
+
+inline long long stoll(const string &__str, size_t *__idx = 0, int __base = 10) {
+  return __gnu_cxx::__stoa(&std::strtoll, "stoll", __str.c_str(), __idx, __base);
+}
+
+inline unsigned long long stoull(const string &__str, size_t *__idx = 0, int __base = 10) {
+  return __gnu_cxx::__stoa(&std::strtoull, "stoull", __str.c_str(), __idx, __base);
+}
+
+// NB: strtof vs strtod.
+inline float stof(const string &__str, size_t *__idx = 0) {
+  return __gnu_cxx::__stoa(&std::strtof, "stof", __str.c_str(), __idx);
+}
+
+inline double stod(const string &__str, size_t *__idx = 0) {
+  return __gnu_cxx::__stoa(&std::strtod, "stod", __str.c_str(), __idx);
+}
+
+inline long double stold(const string &__str, size_t *__idx = 0) {
+  return __gnu_cxx::__stoa(&std::strtold, "stold", __str.c_str(), __idx);
+}
+
+// NB: (v)snprintf vs sprintf.
+
+// DR 1261.
+inline string to_string(int __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(int), "%d", __val);
+}
+
+inline string to_string(unsigned __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(unsigned), "%u", __val);
+}
+
+inline string to_string(long __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(long), "%ld", __val);
+}
+
+inline string to_string(unsigned long __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(unsigned long), "%lu", __val);
+}
+
+inline string to_string(long long __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(long long), "%lld", __val);
+}
+
+inline string to_string(unsigned long long __val) {
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, 4 * sizeof(unsigned long long), "%llu", __val);
+}
+
+inline string to_string(float __val) {
+  const int __n = __gnu_cxx::__numeric_traits<float>::__max_exponent10 + 20;
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, __n, "%f", __val);
+}
+
+inline string to_string(double __val) {
+  const int __n = __gnu_cxx::__numeric_traits<double>::__max_exponent10 + 20;
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, __n, "%f", __val);
+}
+
+inline string to_string(long double __val) {
+  const int __n = __gnu_cxx::__numeric_traits<long double>::__max_exponent10 + 20;
+  return __gnu_cxx::__to_xstring<string>(&std::vsnprintf, __n, "%Lf", __val);
+}
+}
+#endif
+
 namespace toml
 {
 namespace detail
